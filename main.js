671e94cfddf5f2b1a52f7d51670c51a9f15104d3
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2023-08-22 23:15:13 +0100

    Added RAF, consts, gs, sprites, drawing

--- main.js
+++ main.js
@@ -1,45 +1,89 @@
 // JS 13k 2023 entry
 
 // Global constants
-const xmax=640;
-const ymax=360;
+const XMAX=320;
+const YMAX=180;
+const TILESIZE=16;
+const TILESPERROW=12;
+
+const STATEINTRO=0;
+const STATEMENU=1;
+const STATEPLAYING=2;
+const STATENEWLEVEL=3;
+const STATECOMPLETE=4;
+
+const KEYNONE=0;
+const KEYLEFT=1;
+const KEYUP=2;
+const KEYRIGHT=4;
+const KEYDOWN=8;
+const KEYACTION=16;
 
 // Game state
 var gs={
+  // animation frame of reference
+  step:(1/60), // target step time @ 60 fps
+  acc:0, // accumulated time since last frame
+  lasttime:0, // time of last frame
+  fps:0, // current FPS
+  frametimes:[], // array of frame times
+
   // Canvas
-  canvas:null,
+  canvas:null, // Tiles
   ctx:null,
-  scale:1,
+  scanvas:null, // Sprites
+  sctx:null,
+  scale:1, // Changes when resizing window
+
+  // Tilemap image
+  tilemap:null,
+  tilemapflip:null,
 
   // Main character
-  keystate:0,
   x:0, // x position
   y:0, // y position
   vs:0, // vertical speed
   hs:0, // horizontal speed
-  dir:0, //direction (-1=left, 0=none, 1=right)
 
+  // Level attributes
+  level:0, // Level number (0 based)
+  width:0, // Width in tiles
+  height:0, // height in tiles
+  xoffset:0, // current view offset from left (horizontal scroll)
+  yoffset:0, // current view offset from top (vertical scroll)
+
+  // Input
+  keystate:KEYNONE,
+  padstate:KEYNONE,
+  gamepadbuttons:[], // Button mapping
+  gamepadaxes:[], // Axes mapping
+  gamepadaxesval:[], // Axes values
+
+  // Tiles
+  tiles:[], // copy of current level (to allow destruction)
+
+  // Characters
+  chars:[],
+  anim:8, // time until next character animation frame
+
+  // Particles
+  particles:[], // an array of particles for explosion frage, footprint / jump dust
+
+  // Game state
+  state:STATEINTRO, // state machine, 0=intro, 1=menu, 2=playing, 3=complete
+
+  // Timeline for animation
+  timeline:new timelineobj(),
+
+  // Debug flag
   debug:false
 };
-
-// Clear keyboard input state
-function clearinputstate()
-{
-  gs.keystate=0;
-}
-
-// Check if an input is set in keyboard input state
-function ispressed(keybit)
-{
-  return ((gs.keystate&keybit)!=0);
-}
 
 // Handle resize events
 function playfieldsize()
 {
   var height=window.innerHeight;
-  var aspectratio=xmax/ymax;
-  var ratio=xmax/ymax;
+  var ratio=XMAX/YMAX;
   var width=Math.floor(height*ratio);
   var top=0;
   var left=Math.floor((window.innerWidth/2)-(width/2));
@@ -47,90 +91,115 @@ function playfieldsize()
   if (width>window.innerWidth)
   {
     width=window.innerWidth;
-    ratio=ymax/xmax;
+    ratio=YMAX/XMAX;
     height=Math.floor(width*ratio);
 
     left=0;
     top=Math.floor((window.innerHeight/2)-(height/2));
   }
   
-  gs.scale=(height/ymax);
+  gs.scale=(height/YMAX);
 
+  // Tiles
   gs.canvas.style.top=top+"px";
   gs.canvas.style.left=left+"px";
   gs.canvas.style.transformOrigin='0 0';
   gs.canvas.style.transform='scale('+gs.scale+')';
+
+  // Sprites
+  gs.scanvas.style.top=top+"px";
+  gs.scanvas.style.left=left+"px";
+  gs.scanvas.style.transformOrigin='0 0';
+  gs.scanvas.style.transform='scale('+gs.scale+')';
 }
 
-// Update the player key state
-function updatekeystate(e, dir)
+// Draw tile
+function drawtile(tileid, x, y)
 {
-  switch (e.which)
+  // Don't draw tile 0 (background)
+  if (tileid==0) return;
+
+  // Clip to what's visible
+  if (((x-gs.xoffset)<-TILESIZE) && // clip left
+      ((x-gs.xoffset)>XMAX) && // clip right
+      ((y-gs.yoffset)<-TILESIZE) && // clip top
+      ((y-gs.yoffset)>YMAX))   // clip bottom
+    return;
+
+  gs.ctx.drawImage(gs.tilemap, (tileid*TILESIZE) % (TILESPERROW*TILESIZE), Math.floor((tileid*TILESIZE) / (TILESPERROW*TILESIZE))*TILESIZE, TILESIZE, TILESIZE, x-gs.xoffset, y-gs.yoffset, TILESIZE, TILESIZE);
+}
+
+// Draw sprite
+function drawsprite(sprite)
+{
+  // Don't draw sprite 0 (background)
+  if (sprite.id==0) return;
+
+  // Clip to what's visible
+  if (((Math.floor(sprite.x)-gs.xoffset)<-TILESIZE) && // clip left
+      ((Math.floor(sprite.x)-gs.xoffset)>XMAX) && // clip right
+      ((Math.floor(sprite.y)-gs.yoffset)<-TILESIZE) && // clip top
+      ((Math.floor(sprite.y)-gs.yoffset)>YMAX))   // clip bottom
+    return;
+
+  if (sprite.flip)
+    gs.sctx.drawImage(gs.tilemapflip, ((TILESPERROW*TILESIZE)-((sprite.id*TILESIZE) % (TILESPERROW*TILESIZE)))-TILESIZE, Math.floor((sprite.id*TILESIZE) / (TILESPERROW*TILESIZE))*TILESIZE, TILESIZE, TILESIZE,
+      Math.floor(sprite.x)-gs.xoffset, Math.floor(sprite.y)-gs.yoffset, TILESIZE, TILESIZE);
+  else
+    gs.sctx.drawImage(gs.tilemap, (sprite.id*TILESIZE) % (TILESPERROW*TILESIZE), Math.floor((sprite.id*TILESIZE) / (TILESPERROW*TILESIZE))*TILESIZE, TILESIZE, TILESIZE,
+      Math.floor(sprite.x)-gs.xoffset, Math.floor(sprite.y)-gs.yoffset, TILESIZE, TILESIZE);
+}
+
+// Redraw the game world
+function redraw()
+{
+  // Clear the tile canvas
+  gs.ctx.fillRect(0, 0, gs.canvas.width, gs.canvas.height);
+
+  drawtile(99, 0, 0);
+
+  // Clear the sprites canvas
+  gs.sctx.clearRect(0, 0, gs.scanvas.width, gs.scanvas.height);
+}
+
+function rafcallback(timestamp)
+{
+  // First time round, just save epoch
+  if (gs.lasttime>0)
   {
-    case 37: // cursor left
-    case 65: // A
-    case 90: // Z
-      if (dir==1)
-        gs.keystate|=1;
-      else
-        gs.keystate&=~1;
+    // Determine accumulated time since last call
+    gs.acc+=((timestamp-gs.lasttime) / 1000);
 
-      e.preventDefault();
-      break;
+    // If it's more than 15 seconds since last call, reset
+    if ((gs.acc>gs.step) && ((gs.acc/gs.step)>(60*15)))
+      gs.acc=gs.step*2;
 
-    case 38: // cursor up
-    case 87: // W
-    case 59: // semicolon
-      if (dir==1)
-        gs.keystate|=2;
-      else
-        gs.keystate&=~2;
+    // Gamepad support
+    try
+    {
+      if (!!(navigator.getGamepads))
+        gamepadscan();
+    }
+    catch(e){}
 
-      e.preventDefault();
-      break;
+    // Process "steps" since last call
+    while (gs.acc>gs.step)
+    {
+      //update();
+      gs.acc-=gs.step;
+    }
 
-    case 39: // cursor right
-    case 68: // D
-    case 88: // X
-      if (dir==1)
-        gs.keystate|=4;
-      else
-        gs.keystate&=~4;
+    redraw();
 
-      e.preventDefault();
-      break;
-
-    case 40: // cursor down
-    case 83: // S
-    case 190: // dot
-      if (dir==1)
-        gs.keystate|=8;
-      else
-        gs.keystate&=~8;
-
-      e.preventDefault();
-      break;
-
-    case 13: // enter
-    case 32: // space
-      if (dir==1)
-        gs.keystate|=16;
-      else
-        gs.keystate&=~16;
-
-      e.preventDefault();
-      break;
-
-    case 73: // I (for info/debug)
-      if (dir==1)
-        gs.debug=(!gs.debug);
-
-      e.preventDefault();
-      break;
-
-    default:
-      break;
+    // If the update took us out of play state then stop now
+    if (gs.state!=STATEPLAYING)
+      return;
   }
+
+  // Remember when we were last called
+  gs.lasttime=timestamp;
+
+  window.requestAnimationFrame(rafcallback);
 }
 
 // Entry point
@@ -156,13 +225,40 @@ function init()
     e.preventDefault();
   };
 
-  // Set up canvas
-  gs.canvas=document.getElementById("canvas");
+  // Set up tiles canvas
+  gs.canvas=document.getElementById("tiles");
   gs.ctx=gs.canvas.getContext("2d");
+
+  // Set up sprites canvas
+  gs.scanvas=document.getElementById("sprites");
+  gs.sctx=gs.scanvas.getContext("2d");
 
   window.addEventListener("resize", function() { playfieldsize(); });
 
   playfieldsize();
+
+  // Load tilemap
+  gs.tilemap=new Image;
+  gs.tilemap.onload=function()
+  {
+    // Create a horizontally flipped version of the spritesheet
+    // https://stackoverflow.com/questions/21610321/javascript-horizontally-flip-an-image-object-and-save-it-into-a-new-image-objec
+    var c=document.createElement('canvas');
+    var ctx=c.getContext('2d');
+    c.width=gs.tilemap.width;
+    c.height=gs.tilemap.height;
+    ctx.scale(-1, 1);
+    ctx.drawImage(gs.tilemap, -gs.tilemap.width, 0);
+
+    gs.tilemapflip=new Image;
+    gs.tilemapflip.onload=function()
+    {
+      // Start frame callbacks
+      window.requestAnimationFrame(rafcallback);
+    };
+    gs.tilemapflip.src=c.toDataURL();
+  };
+  gs.tilemap.src=tilemap;
 }
 
 // Run the init() once page has loaded
