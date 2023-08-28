15e89e666d04581ec482128a9812dc7a192e4a3a
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2023-08-28 01:23:37 +0100

    Updated level

--- assets/tilemap.tsx
+++ assets/tilemap.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tilemap_2023" tilewidth="16" tileheight="16" tilecount="264" columns="12">
+<tileset name="tilemap_2023" tilewidth="16" tileheight="16" tilecount="168" columns="12">
  <image source="tilemap.png" width="192" height="352"/>
 </tileset>
