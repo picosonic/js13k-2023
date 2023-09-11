0c15743738d389a7e22cfff03d92416f64d5470d
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2023-09-11 23:21:29 +0100

    Updates to shop #28

--- signs.js
+++ signs.js
@@ -1,9 +1,11 @@
 const signs=[
 {"loc":"-1,-1","txt":"[117]Door is locked\nfind a key"},
+{"loc":"-2,-2","txt":"[93]You don't have\nenough money\nto buy this potion"},
 {"loc":"13,10","txt":"Find/buy potions to cure villagers\n\nFind all the treasure and defeat\nall the enemies"},
 {"loc":"3,3","txt":"Beekeeper's house"},
-{"loc":"15,6","txt":"Wizard's house\nand shop"},
+{"loc":"15,6","txt":"Wizard's shop"},
 {"loc":"22,6","txt":"Blacksmith's house"},
 {"loc":"29,8","txt":"Church"},
-{"loc":"23,20","txt":"Castle"}
+{"loc":"23,20","txt":"Castle"},
+{"loc":"12,7","txt":"Welcome to the shop\n\n2 - Coloured potion\n5 - White potion"}
 ];
