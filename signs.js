c7fa78d104862fdc34d5cb0b7c0f5d50657dee4d
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2023-09-12 18:46:17 +0100

    Changed to refresh

--- signs.js
+++ signs.js
@@ -1,7 +1,7 @@
 const signs=[
 {"loc":"-1,-1","txt":"[117]Door is locked\nfind a key"},
 {"loc":"-2,-2","txt":"[93]You don't have\nenough money\nto buy this potion"},
-{"loc":"-3,-3","txt":"Congratulations you have saved the village\nPress on the screen to play again"},
+{"loc":"-3,-3","txt":"Congratulations you have saved the village\nRefresh page to play again"},
 {"loc":"13,10","txt":"Find/buy potions to cure villagers\n\nFind all the treasure and defeat\nall the enemies"},
 {"loc":"3,3","txt":"Beekeeper's house"},
 {"loc":"15,6","txt":"Wizard's shop"},
